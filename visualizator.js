dbf1ea2f034f59f6217cc5e687feabff40af1811
Kelvin Henrique <kelvinh.job@gmail.com>
2023-07-20 16:43:12 -0300

    Update visualizator.js

--- visualizator.js
+++ visualizator.js
@@ -5,7 +5,8 @@ const barHeight = 2;
 const barSpacing = 2;
 const radius = 140;
 const beta = Math.PI * 0.75;
-const URL = "https://radiofg.impek.com/fg";
+//const URL = "https://radiofg.impek.com/fg";
+const URL = "https://stm.srvvox.com.br:7460/stream"
 let started = false;
 
 function start() {
